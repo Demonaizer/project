f5266d6434b98ea8c7699b736f6904e914967083
Ilya <example@gmail.com>
2021-04-10 13:34:39 +0700

    new js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
 "use strict";
 
-// alert('Hello');
+const numberOfFilms = +prompt('Сколько фильмов вы уже помотрели?', '');
 
-// const result = confirm("Are you here?");
-// console.log(result);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-const answer = prompt("Вам есть 18", "18");
-console.log(typeof(answer));
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+b = prompt('На сколько оцените его?', ''),
+c = prompt('Один из последних просмотренных фильмов?', ''),
+d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
